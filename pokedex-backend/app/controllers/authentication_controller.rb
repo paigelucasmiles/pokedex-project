09e3cae70562e090f3346ac77a67d47ae9f36e87
Paige Miles <paige@lucas-miles.com>
2021-01-26 14:58:30 -0700

    login method created

--- pokedex-backend/app/controllers/authentication_controller.rb
+++ pokedex-backend/app/controllers/authentication_controller.rb
@@ -1,7 +1,19 @@
 class AuthenticationController < ApplicationController
+    
+    # skip_before_action :verify_auth, only: [:login]
 
     def login
-        
+        @user = User.find_by username: params[:username]
+
+        if (@user&.authenticate(params[:password]))
+            @token = JWT.encode({
+                user_id: @user.id
+            }, Rails.application.secrets.secret_key_base)
+
+            render json: {token: @token}, status: :ok
+        else 
+            render json: {message: 'Invalid login credentials'}, status: :unauthorized
+        end
     end
 
 end
